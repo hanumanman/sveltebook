d0526169939dcbe1a6265ca235c3fdd3831c0078
itztla <hanumanman37@gmail.com>
2025-05-06 18:30:33 +0700

    fix: types

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
   import { cn } from '$lib/utils';
   import type { Snippet } from 'svelte';
+  import type { HTMLButtonAttributes } from 'svelte/elements';
 
-  interface Props {
+  type ButtonProps = HTMLButtonAttributes & {
     children: Snippet<[]>;
-    onclick?: () => void;
-    type?: 'button' | 'submit' | 'reset';
-    class?: string;
-    style?: string;
-  }
+  };
 
-  let { children, onclick, style, type = 'button', class: className }: Props = $props();
+  let { children, class: className, ...props }: ButtonProps = $props();
 </script>
 
 <button
-  {type}
-  {onclick}
+  {...props}
   class={cn(
-    'bg-burgundy-500 flex cursor-pointer items-center justify-center gap-2 rounded-lg px-3  py-2 text-center text-white transition-all duration-200 hover:inset-shadow-sm hover:brightness-125',
+    'bg-burgundy-500 flex cursor-pointer items-center justify-center gap-2 rounded-lg px-3 py-2 text-center text-white transition-all duration-200 hover:inset-shadow-sm hover:brightness-125',
     className
   )}
-  {style}
 >
   {@render children()}
 </button>
--- src/lib/components/LinkButton.svelte
+++ src/lib/components/LinkButton.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
   import { cn } from '$lib/utils';
   import type { Snippet } from 'svelte';
+  import type { HTMLAnchorAttributes } from 'svelte/elements';
 
-  interface Props {
-    children: Snippet<[]>;
-    href?: string;
-    class?: string;
-  }
+  type LinkButtonProps = HTMLAnchorAttributes & {
+    children?: Snippet<[]>;
+  };
 
-  let { children, href = '/', class: className }: Props = $props();
+  let { children, href = '/', class: className, ...props }: LinkButtonProps = $props();
 </script>
 
 <a
   {href}
+  {...props}
   class={cn(
     'bg-burgundy-500 flex cursor-pointer items-center justify-center gap-2 rounded-lg px-3  py-2 text-center text-white transition-all duration-300 hover:inset-shadow-sm hover:brightness-125',
     className
   )}
 >
-  {@render children()}
+  {#if children}
+    {@render children()}
+  {/if}
 </a>
