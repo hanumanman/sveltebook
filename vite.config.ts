578771a15e22b52d3b4c298b09a1085f867e4af6
itztla <hanumanman37@gmail.com>
2025-05-09 21:35:47 +0700

    add devtoolJson plugin for chromium devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-  plugins: [enhancedImages(), tailwindcss(), sveltekit()]
+  plugins: [enhancedImages(), tailwindcss(), sveltekit(), devtoolsJson()]
 });
